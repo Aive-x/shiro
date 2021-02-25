57540caafbf4b3f20362afbba9c09245972e3d1e
WeiYifan <584344164@QQ.com>
2021-02-25 16:25:38 +0800

    weiyifan-2.25-epidemic twice \n and input change

--- src/main/java/com/springboot/shiro/service/Impl/EpidemicServiceImpl.java
+++ src/main/java/com/springboot/shiro/service/Impl/EpidemicServiceImpl.java
@@ -38,7 +38,7 @@ public class EpidemicServiceImpl implements EpidemicService {
     public List<AreaEpidemic> getAreaEpidemicInformation() throws Exception {
         EpidemicInformation epidemicInformation = this.getEpidemicInformation();
         AreaTree areaTree = epidemicInformation.getAreaTree().stream()
-            .collect(Collectors.toMap(AreaTree::getName, areaTree1 -> areaTree1)).get("中国");
+                .collect(Collectors.toMap(AreaTree::getName, areaTree1 -> areaTree1)).get("中国");
         List<AreaEpidemic> areaEpidemicList = new ArrayList<>();
         areaTree.getChildren().forEach(area -> {
             AreaEpidemic areaEpidemic = new AreaEpidemic();
@@ -54,39 +54,94 @@ public class EpidemicServiceImpl implements EpidemicService {
     public List<String> getBoardEpidemicInformation() throws Exception {
         EpidemicInformation epidemicInformation = this.getEpidemicInformation();
         ChinaTotal chinaTotal = epidemicInformation.getChinaTotal();
+        List<ChinaDayList> chinaDayList = epidemicInformation.getChinaDayList();
+
         Total total = chinaTotal.getTotal();
         Today today = chinaTotal.getToday();
+        ExtData extData =chinaTotal.getExtData();
+
+
 
         String currentDiagnosed;
+        String diagnosed;
         String suspect;
-        String newdeth;
+        String cured;
+        String death;
+        String input;
+        int nowConfirm;
+        int incinput;
+
+        nowConfirm = total.getConfirm() - total.getDead() - total.getHeal();
+        incinput = total.getInput()-chinaDayList.get(chinaDayList.size()-1).getTotal().getInput();
 
         if (today.getConfirm() == null) {
-            currentDiagnosed = "新增确诊:" + "0";
+            currentDiagnosed = "较上日" + "+0" + "\n"
+                    + total.getConfirm() + "\n现有确诊";
+        } else if (today.getStoreConfirm().compareTo(0)==1) {
+            currentDiagnosed = "较上日" + "+" + today.getStoreConfirm() + "\n"
+                    + nowConfirm + "\n现有确诊";
         } else {
-            currentDiagnosed = "新增确诊:" + today.getConfirm();
-        }
-        if (today.getSuspect() == null) {
-            suspect = "新增疑似:" + "0";
-        } else {
-            suspect = "新增疑似:" + today.getSuspect();
-        }
-        if (today.getDead() == null) {
-            newdeth = "新增死亡:" + "0";
-        } else {
-            newdeth = "新增死亡:" + today.getDead();
+            currentDiagnosed = "较上日" + today.getStoreConfirm() + "\n"
+                    + nowConfirm + "\n现有确诊";
         }
 
-        String diagnosed = "累计确诊:" + total.getConfirm();
-        String input = "境外输入:" + total.getInput();
-        String cured = "累计治愈:" + total.getHeal();
-        String death = "累计死亡:" + total.getDead();
+        if (today.getConfirm() == null) {
+            diagnosed = "较上日" + "+0" + "\n"
+                    + total.getConfirm() + "\n累计确诊";
+        } else if (today.getConfirm().compareTo(0)==1) {
+            diagnosed = "较上日" + "+" + today.getConfirm() + "\n"
+                    + total.getConfirm() + "\n累计确诊";
+        } else {
+            diagnosed = "较上日" + today.getConfirm() + "\n"
+                    + total.getConfirm() + "\n累计确诊";
+        }
+
+        if (extData.getIncrNoSymptom() == null) {
+            suspect = "较上日" + "+0" + "\n"
+                    + extData.getNoSymptom() + "\n无症状感染者";
+        } else if (extData.getIncrNoSymptom().compareTo(0)==1) {
+            suspect = "较上日" + "+" + extData.getIncrNoSymptom() + "\n"
+                    + extData.getNoSymptom()  + "\n无症状感染者";
+        } else {
+            suspect = "较上日" + extData.getIncrNoSymptom() + "\n"
+                    + extData.getNoSymptom()  + "\n无症状感染者";
+        }
+
+        if (today.getHeal() == null) {
+            cured = "较上日" + "+0" + "\n"
+                    + total.getHeal() + "\n累计治愈";
+        } else if (today.getHeal().compareTo(0)==1) {
+            cured = "较上日" + "+" + today.getHeal() + "\n"
+                    + total.getHeal() + "\n累计治愈";
+        } else {
+            cured = "较上日" + today.getHeal() + "\n"
+                    + total.getHeal() + "\n累计治愈";
+        }
+
+        if (today.getDead() == null) {
+            death = "较上日" + "+0" + "\n"
+                    + total.getDead() + "\n累计死亡";
+        } else if (today.getDead().compareTo(0)==1) {
+            death = "较上日" + "+" + today.getDead() + "\n"
+                    + total.getDead() + "\n累计死亡";
+        } else {
+            death = "较上日" + today.getDead() + "\n"
+                    + total.getDead() + "\n累计死亡";
+        }
+
+        if (incinput>=0) {
+            input = "较上日" + "+" + incinput + "\n"
+                    + total.getInput() + "\n境外输入";
+        } else {
+            input = "较上日" + incinput + "\n"
+                    + total.getInput() + "\n境外输入";
+        }
+
 
         List<String> boardEpidemicInformation = new ArrayList<>();
         boardEpidemicInformation.add(currentDiagnosed);
         boardEpidemicInformation.add(diagnosed);
         boardEpidemicInformation.add(suspect);
-        boardEpidemicInformation.add(newdeth);
         boardEpidemicInformation.add(input);
         boardEpidemicInformation.add(cured);
         boardEpidemicInformation.add(death);
