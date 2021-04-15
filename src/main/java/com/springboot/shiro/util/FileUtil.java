dcee7c93cd269af41581307f7449ecf5751cef31
xutianhong <736950061@qq.com>
2021-04-15 17:57:59 +0800

    fix-将excel数据都转换为string

--- src/main/java/com/springboot/shiro/util/FileUtil.java
+++ src/main/java/com/springboot/shiro/util/FileUtil.java
@@ -4,6 +4,7 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -60,6 +61,7 @@ public class FileUtil {
                 String[] cells = new String[lastCellNum + 1];
                 for (int j = 0; j < lastCellNum; j++) {
                     XSSFCell cell = row.getCell(j);
+                    cell.setCellType(CellType.STRING);
                     cells[j] = cell.toString();
                 }
                 list.add(cells);
